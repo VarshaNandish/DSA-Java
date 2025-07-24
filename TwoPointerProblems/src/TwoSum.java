8d336dbfb38d8cb2cec599a5c76603e483165e25
Varsha <spvarsha6@gmail.com>
2025-07-25 00:08:05 +0530

    Two sum problems

---
+++ TwoPointerProblems/src/TwoSum.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class TwoSum {
+
+    static boolean twoSum(int[] arr, int target) {
+
+        Arrays.sort(arr);
+        int left =0;
+        int right = arr.length-1;
+
+        while(left<right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target)
+                return true;
+                else if (sum<target)
+                    left++;
+                else
+                    right--;
+
+        }
+        return false;
+
+    }
+    public static void main(String[] args){
+        int[] arr = { 0, -1, 2, -3, 1 };
+        int target = -2;
+
+        if (twoSum(arr, target)) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
