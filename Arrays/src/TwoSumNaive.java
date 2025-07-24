4f138e453ab66c9099a1a48acfbbe56655cef725
Varsha <spvarsha6@gmail.com>
2025-07-25 00:25:02 +0530

    Merge two sorted arrays, three pointer approach

--- Arrays/src/TwoSumNaive.java
+++ Arrays/src/TwoSumNaive.java
@@ -2,7 +2,7 @@ public class TwoSumNaive {
 
     static boolean twoSum(int[] arr, int target){
         int n = arr.length;
-        
+
         for (int i=0; i<n; i++){
             for (int j=i+1; j<n; j++){
                 if (arr[i]+arr[j]==target) {
